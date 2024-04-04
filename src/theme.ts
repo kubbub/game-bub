9b7cee73061e69de1afc2d6c4a4b1ad3e6abac5f
kubbub <jakub.seta93@gmail.com>
2024-04-04 22:05:24 +0200

    dark mode

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initalColorMode: "dark",
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
